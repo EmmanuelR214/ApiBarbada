49b90380701a58d326f2cef301e0504d3b948e31
EmmanuelR214 <127364880+EmmanuelR214@users.noreply.github.com>
2024-06-16 23:00:49 -0600

    Update admin.controllers.js

--- src/controllers/admin.controllers.js
+++ src/controllers/admin.controllers.js
@@ -78,7 +78,7 @@ export const LoginAdmin = async(req, res) =>{
 
 export const TraerReporteVentas = async(req, res) => {
     try {
-        const [result] = await Coonexion.execute('SELECT v.id_venta, u.correo AS nombre_usuario, v.estado_pedido, d.direccion AS descripcion_direccion, v.fecha_venta FROM ventas v JOIN usuarios u ON v.id_usuario = u.id_usuario JOIN direcciones d ON v.id_direccion = d.id_direccion JOIN descripcion_ventas dv ON v.id_venta = dv.id_venta')
+        const result = await Coonexion.execute('SELECT v.id_venta, u.correo AS nombre_usuario, v.estado_pedido, d.direccion AS descripcion_direccion, v.fecha_venta FROM ventas v JOIN usuarios u ON v.id_usuario = u.id_usuario JOIN direcciones d ON v.id_direccion = d.id_direccion JOIN descripcion_ventas dv ON v.id_venta = dv.id_venta')
         res.status(200).json(result)
     } catch (error) {
         console.log(error)
